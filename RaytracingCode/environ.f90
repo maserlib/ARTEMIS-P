9bb418b437adf2bbc559ee6a86afb4b06e6fd951
C. Baskevitch <cbask@laposte.net>
2021-12-02 20:03:41 +0100

    Update environ.f90
    English translation of comments

--- RaytracingCode/environ.f90
+++ RaytracingCode/environ.f90
@@ -7,10 +7,10 @@ MODULE environ
 
 subroutine magneticf(V,B)
 	!----------------------------------------------------------
-	! Calcul du champ magnetique
+	! Magnetic field computation.
 	! CALLING SEQUENCE: call magneticf(V,B)
-	! INPUTS:  V: vecteur position en coordonnées cart.
-	! OUTPUTS: B: vecteur champ magnetique en coord cart. en T
+	! INPUTS:  V: position vector in cartesian coordinates
+	! OUTPUTS: B: magnetic field vector in cartesian coordinate (Tesla)
 	!----------------------------------------------------------
 	real(kind=8), dimension(:,:),intent(in)  :: V
 	real(kind=8), dimension(:,:),intent(out) :: B
@@ -28,10 +28,10 @@ end subroutine magneticf
 
 subroutine density(V,Ne)
 	!-----------------------------------------------------
-	! Calcul de la densite d'electron
+	! Electron density computation
 	! CALLING SEQUENCE: call density(V,Ne)
-	! INPUTS:   V: vecteur position en coordonnées cart.
-	! OUTPUTS: Ne: densite d'electron en cm-3 
+	! INPUTS:   V: position vector in cartesian coordinates
+	! OUTPUTS: Ne: electron density (cm-3)
 	!-----------------------------------------------------
 	real(kind=8),dimension(:,:),intent(in)    :: V
 	real(kind=8), dimension(:), intent(out)   :: Ne
